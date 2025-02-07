fdb002e1aa7cf9f10e7a952eb4be91bd1591b297
Surin Pubalan <77199393+lolsurin@users.noreply.github.com>
2025-02-07 17:41:02 +0800

    added README.md

---
+++ README.md
@@ -0,0 +1,40 @@
+# Challenge #1 Big Bang Solution
+
+The solution generates an array of numbers from 1 to 100, replacing: 
+- Numbers divisible by 3 with "BIG"
+- Numbers divisible by 5 with "BANG"
+- Numbers divisible by both 3 and 5 with "BIGBANG"
+
+## ⚠️ Prerequisites
+Make sure you have the following installed on your system:
+- [Node.js](https://nodejs.org/) (v14+ recommended)
+- [Git](https://git-scm.com/)
+- A terminal (Command Prompt, PowerShell, or Terminal)
+
+## 🛠️ Installation & Setup
+1. Open your terminal and run:
+   ```sh
+   git clone https://github.com/lolsurin/versa.git
+   cd versa
+
+2. Install dependencies by running thre command:
+    ```sh 
+    npm install
+
+3. Compile the Typescript:
+    ```sh 
+    npx tsc
+
+## ⚡Running the Script
+1. Run the script:
+    ```sh
+    npm start
+
+2. The output will be saved to output.json in the project directory.
+
+## 📂 Folder Structure
+
+
+## 📜 Sample Output (output.json)
+
+    ["1","2","BIG","4","BANG","BIG","7","8","BIG","BANG","11","BIG","13","14","BIGBANG" .... all the way to 100]
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,42 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
+const fs = __importStar(require("fs"));
 function genBigBangArray() {
-    var result = [];
-    for (var i = 1; i <= 100; i++) {
+    let result = [];
+    for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             result.push('BIGBANG');
         }
@@ -19,10 +52,10 @@ function genBigBangArray() {
     }
     return result;
 }
-var output = genBigBangArray();
+const output = genBigBangArray();
 //Exception Handling 
 try {
-    fs.writeFileSync('output.json', // write to the JSON String to output.json
+    fs.writeFileSync('output.json', // write the JSON String to output.json
     JSON.stringify(output)); // convert to JSON String
     console.log('Output saved to output.json');
 }
